fe392c65f14a7011b77317fda778f9224d2e9543
Lurey14 <noemy.nmg@gmail.com>
2025-09-25 20:02:40 -0400

    creacion_controller_vistas

--- PrimerParcial/obj/Debug/net8.0/PrimerParcial.AssemblyInfo.cs
+++ PrimerParcial/obj/Debug/net8.0/PrimerParcial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerParcial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48514be93db4f16d7fcb02e7caffd911568ce954")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41172facf14e9926b913ae58db584252573a4fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerParcial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerParcial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
